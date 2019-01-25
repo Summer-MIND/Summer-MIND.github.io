c00fbe0814a6f781c5c8b84d5cae7b2b10f1cfdb
ljchang <luke.j.chang@dartmouth.edu>
2019-01-25 10:51:42 -0500

    fixed dropdown issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,6 +196,19 @@
 
 				});
 
+		$(document).ready(function(){
+					$('.modal').modal();
+					$('.dropdown-trigger').dropdown({
+							inDuration: 300,
+							outDuration: 225,
+							constrain_width: false, // Does not change width of dropdown to that of the activator
+							hover: true, // Activate on hover
+							gutter: 0, // Spacing from edge
+							belowOrigin: true, // Displays dropdown below the button
+							alignment: 'left' // Displays dropdown with edge aligned to the left of button
+					});
+		});
+
 	});
 
 })(jQuery);
